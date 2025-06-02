f4f9fadddf875d2b7e006ab7c2ebf4e272aec04c
narakushi <taina.santana1305@gmail.com>
2025-06-02 16:14:23 -0300

    :sparkles: feat: adicionando listas suspensas sobre tipos de atores e setor principal

--- src/components/layout/select/index.jsx
+++ src/components/layout/select/index.jsx
@@ -1,6 +1,13 @@
 import styles from "./index.module.css";
 
-export const Select = ({ label, name, options, htmlFor, id }) => {
+export const Select = ({
+  label,
+  name,
+  options,
+  optionsValues,
+  htmlFor,
+  id,
+}) => {
   return (
     <div className={styles.containerSelect}>
       <label htmlFor={htmlFor} className={styles.label}>
@@ -8,7 +15,7 @@ export const Select = ({ label, name, options, htmlFor, id }) => {
       </label>
       <select name={name} id={id} className={styles.select}>
         {options.map((option) => (
-          <option value={option} key={option}>
+          <option value={optionsValues || option} key={option}>
             {option}
           </option>
         ))}
--- src/pages/registerOrg/index.jsx
+++ src/pages/registerOrg/index.jsx
@@ -3,8 +3,44 @@ import { Container } from "@/components/layout/container";
 import styles from "./index.module.css";
 import inputs from "@/../public/jsons/inputsOrg.json";
 import { Title } from "@/components/layout/title";
+import axios from "axios";
 
-export default function RegisterOrg() {
+export async function getStaticProps() {
+  const [tipoOrg, tipoSetor] = await Promise.all([
+    await axios.get(process.env.NEXT_PUBLIC_TIPO_ATORES),
+    await axios.get(process.env.NEXT_PUBLIC_TIPO_SETOR),
+  ]);
+
+  return {
+    props: {
+      tipoOrg: tipoOrg.data,
+      tipoSetor: tipoSetor.data,
+    },
+  };
+}
+
+export default function RegisterOrg({ tipoOrg, tipoSetor }) {
+  //separando em um array os nomes
+  const tipos = tipoOrg.map((tipo) => tipo.Nome_Tipo_Ator);
+  const tiposSetor = tipoSetor.map((tipo) => tipo.Nome_Setor);
+
+  //separando os ids
+  const idTipos = tipoOrg.map((tipo) => tipo.ID_Tipo_Ator);
+  const idTipoSetor = tipoSetor.map((tipo) => tipo.ID_Setor);
+
+  //passando os dados vindos das tabelas auxiliares para o json do frontend
+  inputs.map((input) => {
+    if (input.id === "tipo-de-organizacao") {
+      input.options = tipos;
+      input.optionsValues = idTipos;
+    } else if (input.id === "setor-principal-de-atuacao") {
+      input.options = tiposSetor;
+      input.optionsValues = idTipoSetor;
+    } else {
+      input;
+    }
+  });
+
   return (
     <div className={styles.containerRegisterOrg}>
       <Container>
